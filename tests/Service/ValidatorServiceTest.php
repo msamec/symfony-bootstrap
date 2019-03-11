9b4aaed645608a9607d8d0a2280a47f6d701780c
marko <marko.schamec@gmail.com>
2019-03-11 21:41:13 +0100

    test(validator): testing validator service

--- tests/Service/ValidatorServiceTest.php
+++ tests/Service/ValidatorServiceTest.php
@@ -1,17 +1,56 @@
 <?php
 
+use App\Exception\ValidationException;
 use App\Service\ValidatorService;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationList;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ValidatorServiceTest extends TestCase
 {
-    public function testValidate()
+    public function testIfValidationFails()
     {
         $validator = $this->createMock(ValidatorInterface::class);
+
+        $validator->method('validate')
+            ->will(
+                $this->returnValue(
+                    new ConstraintViolationList([
+                        new ConstraintViolation('a', 'b', [], 'c', 'd', 'e', null, 'f')
+                    ])
+                )
+            )
+        ;
 
         $validatorService = new ValidatorService(
             $validator
         );
+
+        $object = new stdClass();
+        $this->expectException(ValidationException::class);
+        $this->expectExceptionMessage('Validation failed');
+
+        $validatorService->validate($object);
+    }
+
+    public function testIfValidationPasses()
+    {
+        $validator = $this->createMock(ValidatorInterface::class);
+
+        $validator->method('validate')
+            ->will(
+                $this->returnValue(
+                    new ConstraintViolationList()
+                )
+            )
+        ;
+
+        $validatorService = new ValidatorService(
+            $validator
+        );
+
+        $object = new stdClass();
+        $this->assertEquals(true, $validatorService->validate($object)) ;
     }
 }
